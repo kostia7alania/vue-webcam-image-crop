b13ff816125bec1413df9b22062dff6560747335
kostia7alania <osetia-alania@mail.ru>
2018-10-03 16:51:42 +0300

    Signed-off-by: kostia7alania <osetia-alania@mail.ru>

--- src/App.vue
+++ src/App.vue
@@ -124,7 +124,7 @@ import image_crop from './image_crop';
     loadCam(){ 
         this.video = this.$refs.video;
         if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-          navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+           navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
               this.video.src = window.URL.createObjectURL(stream);
               this.video.play();
           });
